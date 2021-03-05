a5c159a5d11a028afc365ef51c6942a25f28f459
Bősze Máté <bosze.mate1997@gmail.com>
2021-03-05 01:23:21 +0100

    added a few test functions

--- cogs/test.py
+++ cogs/test.py
@@ -22,5 +22,16 @@ class Test(commands.Cog):
         print(ctx.author)
         await asyncio.sleep(5)
         await ctx.reply('Hello {0.author}'.format(ctx))
+
+    @commands.command(name='countdown', help='counting down from 10 to 0')
+    async def countdown(self, ctx):
+        number = 10
+        msg = await ctx.reply(number)
+        await asyncio.sleep(1.0)
+        while (number != 0):
+            number = number - 1
+            await msg.edit(content=number)
+            await asyncio.sleep(1.0)
+
 def setup(client):
     client.add_cog(Test(client))
